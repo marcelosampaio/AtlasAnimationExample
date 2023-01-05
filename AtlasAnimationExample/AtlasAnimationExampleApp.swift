e5bddc2d2a1fa9f1922cf81c8265a017532a590d
Marcelo Sampaio <marcelosampaiio1965@gmail.com>
2023-01-05 14:36:08 -0300

    Initial Commit

---
+++ AtlasAnimationExample/AtlasAnimationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtlasAnimationExampleApp.swift
+//  AtlasAnimationExample
+//
+//  Created by Marcelo Sampaio on 05/01/23.
+//
+
+import SwiftUI
+
+@main
+struct AtlasAnimationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
